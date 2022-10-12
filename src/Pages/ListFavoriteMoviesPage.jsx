6b5b1050e8b1f0d01aff3c73f9dea35c35192e3e
Achsmboh <maulana.achsanxiipa1@gmail.com>
2022-10-12 08:23:47 +0700

    revisi

--- src/Pages/ListFavoriteMoviesPage.jsx
+++ src/Pages/ListFavoriteMoviesPage.jsx
@@ -27,7 +27,7 @@ function ListFavoriteMoviesPage() {
         <p className="my-10 p-1 text-center text-lg font-bold text-white">My Favorite Movie</p>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
           {favorites.map((data) => (
-            <CardRemove key={data.id} image={data.poster_path} title={data.title} onNavigate={() => this.props.navigate(`/detail/${data.id}`)} removeFavorite={() => handleRemoveFav(data)} />
+            <CardRemove key={data.id} image={data.poster_path} title={data.title} onNavigate={() => props.navigate(`/detail/${data.id}`)} removeFavorite={() => handleRemoveFav(data)} />
           ))}
         </div>
       </div>
